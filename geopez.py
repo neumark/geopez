faa2763daf4d533c187cc5663fd9d1a08173f362
Daniel Varga <daniel.varga@prezi.com>
2013-01-18 15:57:12 +0100

    The xml is touched now.

--- geopez.py
+++ geopez.py
@@ -9,14 +9,13 @@ def add_image(tree,meta,bb) :
 
 # Mocks so far.
 def image_data(imgFilename) :
-    return 0.0,0.0,0.0
+    return { "lat":0.0, "lon":0.0, "time":0.0 }
 def bounding_box(imageData) :
     return (0.0,0.0,1.0,1.0)
 
 def main() :
     assert len(sys.argv)>=3
     xmlFilename = sys.argv[1]
-    imageDirname = sys.argv[2]
 
     tree = read_xml(xmlFilename)
     objects = tree.findall("zui-table/object")
@@ -24,16 +23,22 @@ def main() :
 
     imageData = []
     for imgFilename in sys.argv[2:] :
-	meta = image_data(imgFilename)
-	lat,lon,time = meta
-	imageData.append([imgFilename]+list(meta))
+        meta = image_data(imgFilename)
+        lat,lon,time = meta
+        imageData.append([imgFilename]+list(meta))
 
     bb = bounding_box(imageData)
 
-    for meta in imageData :
-	# imgFilename,lat,lon,time = meta
-	add_image(tree,meta,bb)
+    oidPrefix="111000"
+    for i,meta in enumerate(imageData) :
+        canvasX = 1973.0 # somehow transformed from lat,lon,bb and the canvas bb.
+        canvasY = 1977.0
+        oid = oidPrefix+str(i+1)
+        add_image_to_xml(tree,meta,canvasX,canvasY,oid)
 
     write_xml(tree)
+
+    objects = tree.findall("zui-table/object")
+    sys.stderr.write("Orig num of objs: %d\n" % len(objects))
 
 main()
--- process_xml.py
+++ process_xml.py
@@ -7,12 +7,20 @@ def read_xml(filename) :
     tree = ET.fromstring(data)
     return tree
 
-def add_image(filename,position)
+def add_image_to_xml(tree,meta,canvasX,canvasY,oid) :
+    main = tree.findall("zui-table")
+    assert len(main)==1
+    main = main[0]
+    o = ET.Element('object', {'id':oid, 'x':str(canvasX), 'y':str(canvasY), 'r':'0', 'type':'image', 's':'1.0'} )
+    o.append(ET.Element('source'))
+    main.append(o)
+    print o
+    return tree
 
 def write_xml(tree,filename=None) :
     if filename :
-	f = file(filename,"w")
-	f.write(ET.tostring(tree))
-	f.close()
+        f = file(filename,"w")
+        f.write(ET.tostring(tree))
+        f.close()
     else :
-	print ET.tostring(tree)
+        print ET.tostring(tree)
