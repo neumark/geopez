5604d4e62cdf009a290d93f59715d8b0da451aa2
Ryan Lane <ryanlane@Ryan-Lane-Prezi-MacBook.local>
2013-01-18 17:29:06 +0100

    Initial stiching together

--- findMarker.py
+++ findMarker.py
@@ -1,8 +1,8 @@
 from numpy import *
 import PIL.Image as im
 
-def pixelCoord(url1,url2):
-	im1,im2=im.open(fileName1),im.open(fileName2)
+def pixelCoord(file1,file2):
+	im1,im2=im.open(file1),im.open(file2)
 	difs=sum((array(im1)-array(im2))**2,2)
 	coords=array([coord for coord,elem in ndenumerate(difs)])
 	return average(coords,axis=0,weights=difs.flatten())
--- geopez.py
+++ geopez.py
@@ -1,23 +1,27 @@
 import sys
 import EXIF
+import urllib2
+import map_urls
+import findMarker
 from process_xml import *
 
 class photoMetaData:
-	def __init__(fileName):
-		self.id=fileName
-		f = open(imgFilename,'r')
-		tags = EXIF.process_file(f)
-		f.close()
-		self.lat=tags['GPS GPSLatitude']
-		self.long=tags['GPS GPSLongitude']
-		self.time=tags['GPS GPSTimeStamp']
-	def updateCoord(coord):
-		self.coord=coord
+    def __init__(self, fileName):
+        self.id=fileName
+        f = open(fileName,'r')
+        tags = EXIF.process_file(f)
+        f.close()
+        self.lat=47.507081#tags['GPS GPSLatitude']
+        self.lon=19.045688#tags['GPS GPSLongitude']
+        self.time=tags['GPS GPSTimeStamp']
+        
+    def updateCoord(self, coord):
+        self.coord=coord
 
 def add_image(tree,meta,bb):
     add_image_to_xml(tree,meta)
     return tree
-	
+    
 def bounding_box(imageData):
     return (0.0,0.0,1.0,1.0)
 
@@ -30,26 +34,26 @@ def main():
     sys.stderr.write("Orig num of objs: %d\n" % len(objects))
 
     photoData = [photoMetaData(name) for name in sys.argv[2:]]
-	urlList=mapUrls(photoData)
-	blankMap_contents = urllib2.urlopen(urlList[0]).read()
-	for url,photo in zip(urlList[1:],photoData):
-		markerMap_contents=urllib2.urlopen(url).read()
-		photo.updateCoord(pixelCoord(blankMap_contents,markerMap_contents))
+    urlList=map_urls.map_urls(photoData)
+    blankMap_contents = urllib2.urlopen(urlList[0])
+    for url,photo in zip(urlList[1:],photoData):
+        markerMap_contents=urllib2.urlopen(url)
+        photo.updateCoord(findMarker.pixelCoord(blankMap_contents,markerMap_contents))
 
     print imageData
 
-    bb = bounding_box(imageData)
+    # bb = bounding_box(imageData)
 
-    oidPrefix="111000"
-    for i,meta in enumerate(imageData) :
-        canvasX = 1973.0 # somehow transformed from lat,lon,bb and the canvas bb.
-        canvasY = 1977.0
-        oid = oidPrefix+str(i+1)
-        add_image_to_xml(tree,meta,canvasX,canvasY,oid)
+    # oidPrefix="111000"
+    # for i,meta in enumerate(imageData) :
+    #     canvasX = 1973.0 # somehow transformed from lat,lon,bb and the canvas bb.
+    #     canvasY = 1977.0
+    #     oid = oidPrefix+str(i+1)
+    #     add_image_to_xml(tree,meta,canvasX,canvasY,oid)
 
-    write_xml(tree)
+    # write_xml(tree)
 
-    objects = tree.findall("zui-table/object")
-    sys.stderr.write("Orig num of objs: %d\n" % len(objects))
+    # objects = tree.findall("zui-table/object")
+    # sys.stderr.write("Orig num of objs: %d\n" % len(objects))
 
 main()
